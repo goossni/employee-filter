bed8f2547a0a61aafbc6f161d5ab6b269b528e6d
Nick Goossens <nick.goossens@ordina.be>
2021-11-24 12:47:30 +0100

    [STYLE] styled containers in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import CenteredContainer from "./components/ui/CenteredContainer";
+import styles from "./App.module.scss";
+import MainContainer from "./components/ui/MainContainer.module";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <CenteredContainer className={styles["centered-container"]}>
+        <MainContainer></MainContainer>
+      </CenteredContainer>
+    </div>
+  );
 }
 
 export default App;
