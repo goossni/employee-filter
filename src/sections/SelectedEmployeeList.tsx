51fa8c4e5ed9fbb6a193d5da32e4901230b66436
Nick Goossens <nick.goossens@ordina.be>
2021-11-25 11:47:45 +0100

    [STYLE] style selected employee list with items

--- src/sections/SelectedEmployeeList.tsx
+++ src/sections/SelectedEmployeeList.tsx
@@ -1,14 +1,33 @@
+import AccordeonListItem from "../components/list-items/AccordeonListItem";
+import GeneratedAvatar from "../components/ui/avatars/GeneratedAvatar";
 import Topbar from "../components/ui/containers/Topbar";
+import MaterialIcon from "../components/ui/icons/MaterialIcon";
 import GeneralList from "../components/ui/lists/GeneralList";
+import UI_COLORS from "../constants/ui-colors";
 
 import styles from "./SelectedEmployeeList.module.scss";
 
 const SelectedEmployeeList = () => {
+  const avatar = (
+    <GeneratedAvatar
+      className={styles.avatar}
+      name="Nick Goossens"
+      color={UI_COLORS.RED.primaryColor}
+    />
+  );
   return (
     <GeneralList className={styles["selected-employee-list"]}>
       <Topbar>
         <h2>Selected Employees</h2>
       </Topbar>
+      <div className={styles["list-item-container"]}>
+        <MaterialIcon className={styles["close-button"]} face="close" />
+        <AccordeonListItem
+          title="Nick Goossens"
+          subtitle="Frontend Developer"
+          sideElement={avatar}
+        />
+      </div>
     </GeneralList>
   );
 };
